f057eced9a6c269c73d660259d6aeb2dd0556306
hanu-ram <hanumanthhanuram17@gmail.com>
2023-02-22 21:00:02 +0530

    test method commented

--- src/main/java/com/hanu/exceptionhandlers/AccountIsNotFoundException.java
+++ src/main/java/com/hanu/exceptionhandlers/AccountIsNotFoundException.java
@@ -1,6 +1,5 @@
 package com.hanu.exceptionhandlers;
 
-
 public class AccountIsNotFoundException extends Exception {
 	String msg;
 
@@ -16,7 +15,9 @@ public class AccountIsNotFoundException extends Exception {
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
-	
-	
+
+	// public void test() {
+
+	// }
 
 }
